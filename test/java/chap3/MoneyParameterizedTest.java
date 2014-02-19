4694621f6fedb5825d263664b6d63a6ca78eed51
Nischint Ramesh <nischintr@gmail.com>
2014-02-18 19:55:18 -0800

    restructuring dirs

---
+++ test/java/chap3/MoneyParameterizedTest.java
@@ -0,0 +1,29 @@
+package chap3;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+
+@RunWith(JUnitParamsRunner.class)
+public class MoneyParameterizedTest {
+	
+	private static final Object[] getMoney() {
+		return new Object[] {
+			new Object[] {10, "USD"},
+			new Object[] {15, "EUR"}
+		};
+	}
+	
+	@Test
+	@Parameters(method = "getMoney")
+	public void constructorShouldSetAmountAndCurrency(int amount, String currency) {
+		Money money = new Money(amount, currency);
+		
+		assertEquals(amount, money.getAmount());
+		assertEquals(currency, money.getCurrency());
+	}
+}
