8767cba577e106a3c4aaf46317e81010141c33e5
Nischint Ramesh <nischintr@gmail.com>
2014-02-18 17:15:42 -0800

    initial commit, adding first class and related testclasses

---
+++ test/java/chap3/MoneyTest.java
@@ -0,0 +1,16 @@
+package chap3;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MoneyTest {
+
+	@Test
+	public void constructorShouldSetAmountAndCurrency() {
+		Money money = new Money(10, "USD");
+
+		assertEquals(10, money.getAmount());
+		assertEquals("USD", money.getCurrency());
+	}
+}
